6d6dea4e255e58399a365f2c02bac0414690d019
Alex Bouma <me@alexbouma.me>
2019-01-11 20:10:19 +0100

    Update composer and install prestissimo in build

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM stayallive/php:7.3
 
+RUN composer self-update --no-progress && \
+    composer global require hirak/prestissimo
+
 ADD composer-install.sh /bin/
 RUN chmod +x /bin/composer-install.sh
 
--- composer-install.sh
+++ composer-install.sh
@@ -5,9 +5,6 @@ set -e
 # Enable the zip extension for composer
 docker-php-ext-enable zip > /dev/null
 
-# Make sure composer is up-to-date
-composer self-update --quiet --no-progress
-
 # Optionally set the github oauth token
 if [[ ! -z "${PLUGIN_GITHUB_TOKEN}" ]]; then
     composer config -g github-oauth.github.com ${PLUGIN_GITHUB_TOKEN}
@@ -15,8 +12,8 @@ fi
 
 # Install prestissimo to parallelize composer install downloads
 # Can be disabled by setting the `parallel` setting to false
-if [[ -z "$PLUGIN_PARALLEL" || "$PLUGIN_PARALLEL" == "true" ]]; then
-    composer global require --quiet hirak/prestissimo
+if [[ ! -z "$PLUGIN_PARALLEL" && "$PLUGIN_PARALLEL" == "false" ]]; then
+    composer global remove --quiet hirak/prestissimo
 fi
 
 # Run composer install (what we came for)
