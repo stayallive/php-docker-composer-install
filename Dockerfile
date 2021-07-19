fe532db246fa0a95e007f8a79a6b573790909c00
Alex Bouma <me@alexbouma.me>
2021-07-19 20:46:13 +0200

    Remove prestissimo composer plugin

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM stayallive/php:7.3
 
-RUN composer self-update --no-progress && \
-    composer global require hirak/prestissimo
+RUN composer self-update --no-progress
 
 ADD composer-install.sh /bin/
 RUN chmod +x /bin/composer-install.sh
--- composer-install.sh
+++ composer-install.sh
@@ -10,11 +10,5 @@ if [[ ! -z "${PLUGIN_GITHUB_TOKEN}" ]]; then
     composer config -g github-oauth.github.com ${PLUGIN_GITHUB_TOKEN}
 fi
 
-# Install prestissimo to parallelize composer install downloads
-# Can be disabled by setting the `parallel` setting to false
-if [[ ! -z "$PLUGIN_PARALLEL" && "$PLUGIN_PARALLEL" == "false" ]]; then
-    composer global remove --quiet hirak/prestissimo
-fi
-
 # Run composer install (what we came for)
 composer install --prefer-dist --no-progress --no-scripts --ignore-platform-reqs
